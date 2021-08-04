2d14229535f67da9e373de9327d5a58a99571fde
cesaraugustodev2 <csantos3600@gmail.com>
2021-08-04 10:32:28 -0300

    Modificado arquivo main, criado novos metodos

--- Pokemon_class.py
+++ Pokemon_class.py
@@ -4,6 +4,7 @@ Created on Sun May 23 13:04:10 2021
 
 @author: Cesar
 """
+# classe pokemon
 
 
 class Pokemon():
@@ -19,14 +20,61 @@ class Pokemon():
         self.spd = spd
         self.moves = moves
 
+#serializar o Pokemon
     def __repr__(self):
        return "Name: %s\nType: %s \nHP: %s \nlv: %s \nMoves:\n%s" % (self.name, self.nature, self.hp, self.lv, self.moves)
 
+    def getname(self):
+        return self.name
+
+    def getlv(self):
+        return self.lv
+
+    def getnature(self):
+        return self.nature
+
+    def setname(self, name):
+        self.name = name
+
+    def getlv(self):
+        return self.lv
+    def gethp(self):
+        return self.hp
+
+    def getnature(self):
+        return self.nature
+    #metodo para mostrar o movelist
+
     def show_moves(self):
-       print(self.name + " Knows: \n")
-       for m in pikachu.moves:
-          print(m[0] + "\nDamage: "+str(m[1])+"\n" +"Type: " + str(m[2])+"\n" )
+       print(self.name + " Pode usar: \n")
+       for m in self.moves:
+          print(m[0] + "\nDamage: "+str(m[1])+"\n" + "Type: " + str(m[2])+"\n")
+          #metodo para atacar n = movimento dentro do array
+    def attack(self,n):
+          print(self.name + " Atacou com "+str(self.moves[n][0])+" Causou "+str(self.moves[n][1])+" de dano")
+     #metodo para atacar n = movimento dentro do array
+    def damage(self,n):
+        if self.hp>n:
+           print(self.name + " sofreu  "+str(n)+" de dano")
+           self.hp = self.hp-n
+           print("HP restante: "+str(self.hp))
+        else:
+          print(self.name+" desmaiou")
+          exit()
+#metodo para subir de nivel
+    def level_up(self):
+        try:
+            self.lv += 1
+        except Exception as e:
+            return print("Erro ao evoluir Pokemon: ", e)
+        else:
+            print("Parabéns! %s Subiu para o lv %d" % (self.name, self.lv))
+#metodo para mostrar informações do pokemon
+    def pokedex(self):
+        print("Pokedex: ")
+        print(self.name+" Tipo: "+self.nature + " Lv: "+str(self.lv)+"\n")
+        print("Lista de Movimentos: ")
+        for m in self.moves:
+                print(m[0] + "\nDamage: "+str(m[1])+"\n" + "Type: " + str(m[2])+"\n")
 
 
-pikachu = Pokemon('Pikachu', 'Eletric', '1', 25, 30, 15, 50, 25, 50, [['Tackle',25, "Normal"], ['Body Slam',25, "Normal"], ['Thundershock',50, "Eletric"],['Water',60, "Water"]])
-pikachu.show_moves()
--- main.py
+++ main.py
@@ -4,4 +4,40 @@ Created on Sun May 23 13:03:15 2021
 
 @author: Cesar
 """
-import Pokemon
+from Pokemon_class import Pokemon
+#cria uma instancia de um pokemon
+p = Pokemon('Pikachu', 'Eletric', 1, 25, 30, 15, 50, 25, 50, [['Tackle', 25, "Normal"], [
+                  'Body Slam', 25, "Normal"], ['Thundershock', 50, "Eletric"], ['Water', 60, "Water"]])
+#cria uma instancia de um pokemon
+c = Pokemon('Charmander', 'Fire', 1, 35, 20, 10, 20, 15, 45, [['Tackle', 25, "Normal"], [
+                  'Body Slam', 25, "Normal"], ['Ember', 50, "Fire"], ['Smokescreen ', 60, "Normal"]])
+
+print(c.getname())
+print(c.getlv())
+
+c.damage(5)
+c.attack(0)
+c.attack(2)
+c.level_up()
+print("\n")
+c.damage(5)
+p.damage(5)
+p.attack(0)
+p.attack(2)
+print("\n")
+p.level_up()
+c.damage(5)
+c.attack(0)
+c.attack(1)
+c.damage(5)
+print("\n")
+c.attack(3)
+c.attack(3)
+c.level_up()
+c.attack(3)
+c.damage(5)
+print("\n")
+c.damage(5)
+c.damage(5)
+c.damage(20)
+print(c.gethp())
